84e4f1b0ead31d0f5e9b44d410c5fc8ef70e82e8
nashfisch <Nash.Fisch88@gmail.com>
2023-07-06 23:51:24 -0600

    Reworked the conversion function. Need to figure out what's going on with the comparing num > 0.

--- main.py
+++ main.py
@@ -18,41 +18,54 @@ def switchRep():
         entryPrompt.config(text = 'Enter a decimal number: ')
 
 def getEntry():
-    global entryPrompt
-    newEntry = entryPrompt.get()
-    return newEntry
+    return field.get()
 
 # Function to switch a decimal number into binary
-def decToBin(num):
-    binRep = 0
-    base = 1
+def conversion(num):
+    if binary == False:
+        binRep = 0
+        base = 1
 
-    while num > 0:
-        rem = num % 2
-        binRep += rem * base
-        num //= 2
-        base *= 10
+        while num > 0:
+            rem = num % 2
+            binRep += rem * base
+            num //= 2
+            base *= 10
 
-    return binRep
-    
+        return binRep
+
+    elif binary == True:
+        bit = 0
+        decRep = 0
+        i = 0
+        while num > 0:
+            bit = num % 10
+            if bit == 1 or bit == 0:
+                decRep += bit * 2 ** i
+                num //= 10
+                i += 1
+        return decRep
+
+
 
 
 # Entry field
 entryPrompt = tk.Label(root, text = 'Enter a decimal number: ')
 entryPrompt.grid(column = 0, row = 1)
-txt = tk.Entry(root, width = 10)
-txt.grid(column = 1, row = 1)
 
+
+field = tk.Entry(root, width = 10)
+field.grid(column = 1, row = 1)
 
 
 arrow = tk.Label(root, text = '-->')
 arrow.grid(column = 2, row = 1)
 
 
-convert = tk.Button(root, text = 'Convert', bg = 'lime')
+convert = tk.Button(root, text = 'Convert', bg = 'lime', command = lambda: conversion(getEntry()))
 convert.grid(column = 1, row = 2)
 
-switch = tk.Button(root, text = 'Switch', command = switchRep, bg = 'red')
+switch = tk.Button(root, text = 'Switch', bg = 'red', command = switchRep)
 switch.grid(column = 2, row = 2)
 
 
